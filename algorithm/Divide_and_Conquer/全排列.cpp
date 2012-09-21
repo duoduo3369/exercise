ca011ba5c8c0cccfa3ca909a38bfbc2103dca532
= <=>
2012-09-21 12:54:54 +0800

    全排列

---
+++ algorithm/Divide_and_Conquer/全排列.cpp
@@ -0,0 +1,45 @@
+
+#include  <iostream>
+#include <vector>
+using namespace std;
+
+template<class Type>
+void permutation(Type array[],int first,int last)
+{
+    if(first == last)
+    {
+        for( int i = 0; i < last ; ++i )
+        {
+            cout << array[i] << " ";
+        }
+        cout << endl;
+
+    }
+    else
+    {
+        for( int i = first; i < last ; ++i )
+        {
+            swap(array[first],array[i]);
+            permutation(array,first+1,last);
+            swap(array[first],array[i]);
+        }
+
+    }
+}
+template<class Type>
+void swap(Type &a,Type &b)
+{
+    Type temp = a;
+    a = b;
+    b = temp;
+}
+
+
+int main(int argc, char *argv[])
+{
+    int a[4] = {1,2,3,4};
+    permutation(a,0,4);
+    char b[4] = {'a','b','c','d'};
+    permutation(b,0,4);
+    return 0;
+}
---
+++ algorithm/Divide_and_Conquer/数字求和（找零钱）.cpp
@@ -0,0 +1,29 @@
+#include  <iostream>
+
+using namespace std;
+
+int change(int number,int denomination)
+{
+    cout << number << " " << denomination << endl;
+    if(number == 0)
+    {
+        return 1;
+    }
+    else if(number < 0 || denomination <= 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return change(number,denomination-1) + change(number-denomination,denomination);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int count = change(6,6);
+    char* star = "*****************";
+    cout << star << endl
+         << count << endl;
+    return 0;
+}
